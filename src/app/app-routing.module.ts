7ed5155ba5d7c28f2ea2f45538f5f830d0d643ff
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2021-01-24 22:23:04 -0300

    Ex.6 - Rotas para a aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BurguerBuilderComponent } from './burguer-builder/burguer-builder.component';
+import { BurguerListComponent } from './burguer-list/burguer-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create', component: BurguerBuilderComponent },
+  { path: 'list', component: BurguerListComponent },
+  { path: '**', component: BurguerBuilderComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
